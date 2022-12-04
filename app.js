334f63ac009a44d10c47cc0c9293f220428f21b6
Enes Uraz <axeprime91@gmail.com>
2022-12-04 09:15:47 +0300

    add navbar

---
+++ app.js
@@ -0,0 +1,26 @@
+const menu = document.querySelector('.fa-bars');
+const navbar = document.querySelector('.nav-list');
+const link = document.querySelectorAll('.nav-list li a');
+
+menu.addEventListener('click',function(){
+    menu.classList.toggle('fa-close');
+    navbar.classList.toggle('active');
+});
+
+
+link.forEach(item =>{
+    item.addEventListener('click',function(){
+        navbar.classList.remove('active');
+        menu.classList.remove('fa-close');
+        activateLink(item)
+    })
+});
+
+
+
+const activateLink = (value) =>{
+    link.forEach(item =>{
+        item.classList.remove('active');
+    });
+    value.classList.toggle('active');
+}
