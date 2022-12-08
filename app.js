f1b45b41917c6bd3df21ae5fb303ba5725a374a2
Enes Uraz <axeprime91@gmail.com>
2022-12-08 10:01:43 +0300

    create contact section

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-const menu = document.querySelector('.fa-bars');
-const navbar = document.querySelector('.nav-list');
-const link = document.querySelectorAll('.nav-list li a');
+const menu = document.querySelector(".fa-bars");
+const navbar = document.querySelector(".nav-list");
+const link = document.querySelectorAll(".nav-list li a");
 
-menu.addEventListener('click',function(){
-    menu.classList.toggle('fa-close');
-    navbar.classList.toggle('active');
+menu.addEventListener("click", function () {
+  menu.classList.toggle("fa-close");
+  navbar.classList.toggle("active");
 });
 
-
-link.forEach(item =>{
-    item.addEventListener('click',function(){
-        navbar.classList.remove('active');
-        menu.classList.remove('fa-close');
-        activateLink(item)
-    })
+link.forEach((item) => {
+  item.addEventListener("click", function () {
+    navbar.classList.remove("active");
+    menu.classList.remove("fa-close");
+    activateLink(item);
+  });
 });
 
-
-
-const activateLink = (value) =>{
-    link.forEach(item =>{
-        item.classList.remove('active');
-    });
-    value.classList.toggle('active');
-}
+const activateLink = (value) => {
+  link.forEach((item) => {
+    item.classList.remove("active");
+  });
+  value.classList.toggle("active");
+};
