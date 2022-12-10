371c7cd4b11936f0c34db48e829eb8f7f328abec
Enes Uraz <axeprime91@gmail.com>
2022-12-10 10:20:15 +0300

    add review and footer section,create scroll activate logic revisit all codes last time and add comments

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const menu = document.querySelector(".fa-bars");
 const navbar = document.querySelector(".nav-list");
 const link = document.querySelectorAll(".nav-list li a");
+const sections = document.querySelectorAll('section')
 
+
+/* link activate */
 menu.addEventListener("click", function () {
   menu.classList.toggle("fa-close");
   navbar.classList.toggle("active");
@@ -15,6 +18,25 @@ link.forEach((item) => {
   });
 });
 
+
+/* link activate when window scroll load */
+
+window.addEventListener('scroll',function(){
+    sections.forEach((item) =>{
+      const height = item.clientHeight;
+      const offset = item.getBoundingClientRect().top + this.window.scrollY - 200;
+      const top = window.scrollY;
+      if(top >= offset && top < offset + height){
+        const id = item.getAttribute('id');
+        const element =document.querySelector('[href="#' + id + '"]');
+        activateLink(element);
+      }
+    })
+})
+
+
+
+/* custom activate link */
 const activateLink = (value) => {
   link.forEach((item) => {
     item.classList.remove("active");
